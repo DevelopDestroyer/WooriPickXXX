9a5ddf0a04787192e30bac6bdc05cb5c781857a9
developdestroyer <developdestroyer@gmail.com>
2021-04-13 00:38:17 +0900

    feat : excutable jar 환경 구성  - jar 패키징 환경 구성  - CORS 해제

---
+++ back/src/main/java/com/pickxxx/woori/wooripickxxx/configuration/WebMvcConfigurerImpl.java
@@ -0,0 +1,14 @@
+package com.pickxxx.woori.wooripickxxx.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfigurerImpl implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") //모든 요청에 대해서
+                .allowedOrigins("*"); //허용할 오리진들
+    }
+}
